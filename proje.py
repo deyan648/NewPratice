90a85f893f494ac39b1592707d11cf3e07ae56d6
deyan648 <139585975+deyan648@users.noreply.github.com>
2023-07-15 20:20:16 +0200

    Add files via upload

---
+++ proje.py
@@ -0,0 +1,9 @@
+# Projedeki ilk değişiklik yapıldı.
+print ("Projede 1.değiştirme yapıldı")
+
+# Projedeki 2. değişiklik.
+print ("Projede 2.geliştime yapıldı")
+
+# Projedeki 3. değişiklik.
+print ("Projedeki 3. geliştirme")
+
